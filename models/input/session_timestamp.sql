1db59977274cfe32542894e9bab4f1042c9e7f00
RitikaMNeema <92691747+RitikaMNeema@users.noreply.github.com>
2025-11-04 22:21:52 -0800

    Update session_timestamp.sql

--- models/input/session_timestamp.sql
+++ models/input/session_timestamp.sql
@@ -1,8 +1,3 @@
-with session_timestamp as (
-    select
-        sessionId,
-        ts
-    from {{ source('raw','session_timestamp') }}
-    WHERE sessionId IS NOT NULL
-)
-select * from session_timestamp
+SELECT sessionId, ts
+FROM {{ source('raw','session_timestamp') }}
+WHERE sessionId IS NOT NULL
